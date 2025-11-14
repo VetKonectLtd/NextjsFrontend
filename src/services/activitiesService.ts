03e0780ea174dc12177d395f2d325924826ef9a1
petnacode <peternnadi1999@gmail.com>
2025-11-14 21:22:12 +0100

    stopped the notification endpoint from making request for unauthenticated user

--- src/services/activitiesService.ts
+++ src/services/activitiesService.ts
@@ -1,6 +1,7 @@
 import { useGet } from "@/lib/hooks";
 import { ACTIVITIES } from "@/lib/api-constants";
 import { Activity } from "@/types";
+import Cookies from "js-cookie";
 
 // any service using hooks
 export const useActivitiesService = () => {
@@ -8,39 +9,42 @@ export const useActivitiesService = () => {
 		const queryParams = new URLSearchParams();
 		queryParams.append("page", page.toString());
 
-        const url = `${ACTIVITIES.GET_ACTIVITIES}?${queryParams.toString()}`;
-		return useGet<Activity>(
-			["activity", page.toString()],
-			url,
-			{
-				enabled,
-				keepPreviousData: true,
-				staleTime: 0,
-			},
-		);
+		const url = `${ACTIVITIES.GET_ACTIVITIES}?${queryParams.toString()}`;
+		return useGet<Activity>(["activity", page.toString()], url, {
+			enabled,
+			keepPreviousData: true,
+			staleTime: 0,
+		});
 	};
 
 	const useGetNotification = (enabled: boolean = false) => {
+		const token = Cookies.get("auth-token");
+
+		const isEnabled = Boolean(token) && Boolean(enabled);
 		return useGet<any>(
 			["notifications"],
 			`${ACTIVITIES.GET_USER_NOTIFICATION}`,
 			{
-				enabled,
+				enabled: isEnabled,
 				staleTime: 0,
 			},
 		);
 	};
 
 	const useGetUserNotificationById = (Id: string, enabled: boolean = false) => {
+		const token = Cookies.get("auth-token");
+
+		const isEnabled = Boolean(token) && Boolean(enabled) && Boolean(Id);
+
 		return useGet<any>(
 			["notification", Id],
 			`${ACTIVITIES.GET_USER_NOTIFICATIONS_BY_ID(Id)}`,
 			{
-				enabled,
+				enabled: isEnabled,
 				staleTime: 0,
 			},
 		);
-	}
+	};
 
 	return {
 		useGetActivities,
